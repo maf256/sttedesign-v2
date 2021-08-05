9cf03cc179c7f9f80f468404988f96df74632fa7
maf256 <maf256@gmail.com>
2021-08-05 22:51:35 +0200

    Fixed menu

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function hamOnClick(event) {
     let menuContainer = document.querySelectorAll('.menu-container')[0]
+    let body = document.getElementsByTagName('body')[0]
     let ham = document.querySelectorAll('.ham')[0]
     if (ham.innerText === '☰') {
         ham.innerText = '╳'
         menuContainer.setAttribute('id', "ham-show");
+        body.style.overflow = 'hidden';
     } else {
+        body.style.overflow = 'auto';
         ham.innerText = '☰'
         menuContainer.setAttribute('id', "");
     }
